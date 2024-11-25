38e7474c036350462fd68e37b8f6ebd9b8a24268
Max Charrier <max@puffer.fish>
2024-11-25 15:13:15 +0100

    i must enjoy torturing myself
    Signed-off-by: Max Charrier <max@puffer.fish>

--- hack.ml
+++ hack.ml
@@ -3,6 +3,7 @@
 #use "tools.ml"
 
 open Printf
+open List
 
 let parse_files n r =
 	let rec aux acc i =
@@ -18,44 +19,43 @@ let parse_files n r =
 			in
 			printf "leak %s revision %d parsed as %s\n" n i parsed_name;
 			(* construct the list with each file revision *)
-			aux (parsed_name :: acc) (i+1)
+			aux (parsed_name::acc) (i+1)
 	in
 	aux [] 1
 
 let concatenate_files f =
 	let rec aux acc fl c =
-		match fl with
-		| [] ->
+		if fl = [] then begin
 			printf "total leaked entries=%d\n" c;
 			acc
-		| hd::tl ->
+		end else begin
 			(* read each file entry in the list *)
-			let data,e = read_data_file hd in
-			printf "reading file %s with %d entries\n" hd e;
+			let data,e = read_data_file (hd fl) in
+			printf "reading file %s with %d entries\n" (hd fl) e;
 			(* append new readed data to the accumulated list *)
-			aux (data @ acc) tl (c+e)
+			aux (data@acc) (tl fl) (c+e)
+		end
 	in
 	aux [] f 0
 
 let sanitize_data l =
 	let rec mem x acc =
-		match acc with
-		| [] -> false
-		| hd::tl -> hd = x || mem x tl
+		if acc = [] then false
+		else hd acc = x || mem x (tl acc)
 	in
 	let rec aux acc c =
-		match acc with
-		| [] ->
+		if acc = [] then begin
 			printf "total entries removed=%d\n" c;
 			acc
-		| hd::tl ->
+		end else begin
 			(* return if head element is already present in the tail list *)
-			if mem hd tl then begin
+			if mem (hd acc) (tl acc) then begin
 				(*printf "remove %s entry\n" (fst hd);*)
-				aux tl (c+1)
+				aux (tl acc) (c+1)
 			end else
 				(* go recursively to the end of the list *)
-				hd :: (aux tl c)
+				(hd acc)::(aux (tl acc) c)
+		end
 	in
 	aux l 0
 
@@ -74,44 +74,49 @@ let get_data_leak app rev =
 let find_credentials_with_username login apps =
 	let res = ref "" in
 	let rec find_data acc =
-		match acc with
-		| [] -> ()
-		| (username,password)::tl ->
+		if acc = [] then ()
+		else begin
+			let (username,password) = hd acc in
 			if username = login then begin
 				printf "username %s password %s\n" username password;
 				if password = !res then
 					printf "USERNAME %s SAME PASSWORD %s\n" username password;
 				res := password
 			end;
-			find_data tl
+			find_data (tl acc)
+		end
 	in
 	let rec aux acc =
-		match acc with
-		| [] -> ()
-		| (app,rev)::tl ->
+		if acc = [] then ()
+		else begin
+			let (app,rev) = hd acc in
 			printf "analyze leak %s with %d revision\n" app rev;
 			find_data (get_data_leak app rev);
-			aux tl
+			aux (tl acc)
+		end
 	in
 	aux apps
 
 let get_credentials_with_password pw apps =
 	let rec find_data acc login =
-		match acc with
-		| [] -> ()
-		| (username,password)::tl ->
+		if acc = [] then ()
+		else begin
+			let (username,password) = hd acc in
 			if password = pw then begin
+				let (username,password) = hd acc in
 				printf "password match with username %s\n" username;
-				find_data tl (username :: login)
+				find_data (tl acc) (username::login)
 			end;
-			find_data tl login
+			find_data (tl acc) login
+		end
 	in
 	let rec aux acc =
-		match acc with
-		| [] -> ()
-		| (app,rev)::tl ->
+		if acc = [] then ()
+		else begin
+			let (app,rev) = hd acc in
 			printf "analyze leak %s with %d revision\n" app rev;
 			find_data (get_data_leak app rev) [];
-			aux tl
+			aux (tl acc)
+		end
 	in
 	aux apps
