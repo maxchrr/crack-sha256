53c832b3ecfe2820d5ba4b3e3748356c3ec1d3c0
Max Charrier <max@puffer.fish>
2024-12-06 11:29:08 +0100

    let's finish what I did while drugged out
    Signed-off-by: Max Charrier <max@puffer.fish>

--- hack.ml
+++ hack.ml
@@ -8,7 +8,43 @@ open List
 (* type for user' credentials *)
 type t = { app : string; id : string; pw : string }
 
-let match_by_password
+let find_by_login
+	(data: (string * string) list)
+	(res : (string * string) list)
+	(clear_id : string)
+	(clear_pw : string)
+: (string * string) list =
+	let rec aux acc lst =
+		if lst = [] then acc
+		else begin
+			let (id,pw) = hd lst in
+			if id = clear_id && pw = hash_password clear_pw then begin
+				printf "------------------------------\n";
+				printf "USER CREDENTIALS FOUND!\n";
+				printf "id: %s\nhash pw: %s\n" id pw;
+				printf "clear pw: %s\n" clear_pw;
+				printf "------------------------------\n";
+				aux ((id,pw)::acc) (tl lst)
+			end else aux acc (tl lst)
+		end
+	in
+	aux res data
+
+let crack_with_clear_data
+	(clear_data : (string * string) list)
+	(data : (string * string) list)
+: (string * string) list =
+	let rec aux acc lst =
+		if lst = [] then acc
+		else begin
+			let (id,pw) = hd lst in
+			printf "id: %s\npw: %s\n" id pw;
+			aux (find_by_login acc data id pw) (tl lst)
+		end
+	in
+	aux [] clear_data
+
+let find_by_password
 	(data : (string * string) list)
 	(res : (string * string) list)
 	(pwc : string)
@@ -39,41 +75,12 @@ let crack_with_wordlist
 		if lst1 = [] && lst2 = [] then acc
 		else aux
 			(* pass on each data entry to test current password *)
-			(match_by_password data acc (hd lst1) (hd lst2))
+			(find_by_password data acc (hd lst1) (hd lst2))
 			(* operations on wl and encrypted_wl at same time *)
 			(tl lst1)
 			(tl lst2)
 	in
 	aux [] wl encrypted_wl
-
-let crack_with_clear_data
-	(clear_data : (string * string) list)
-	(data : (string * string) list)
-: (string * string) list =
-	let rec find_in_data acc lst cred =
-		if lst = [] then acc
-		else
-			let (id,pw) = hd lst in
-			if id = fst cred && hash_password pw = snd cred then begin
-				printf "------------------------------\n";
-				printf "CREDENTIAL MATCH\n";
-				printf "id: %s\nclear pw: %s\n" id pw;
-				printf "hash pw: %s\n" (snd cred);
-				printf "------------------------------\n";
-				find_in_data ((id,pw)::acc) (tl lst) cred
-			end else find_in_data acc (tl lst) cred
-	in
-	let rec aux acc lst =
-		if lst = [] then begin
-			printf "\n%d CREDENTIAL MATCH\n" (length acc);
-			acc
-		end else begin
-			let (id,pw) = hd lst in
-			printf "id: %s\npw: %s\n" id pw;
-			aux (find_in_data acc data (id,pw)) (tl lst)
-		end
-	in
-	aux [] clear_data
 
 (*let formalize_result
 	(app : string)
